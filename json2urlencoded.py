32d2a3df3c27717ef29016ed22177724b74b5a9a
LiAN <46818546+Li4N@users.noreply.github.com>
2024-02-13 21:43:16 +0100

    Update json2urlencoded.py

--- json2urlencoded.py
+++ json2urlencoded.py
@@ -1,9 +1,11 @@
 import json
 import urllib.parse
 
-
 jsonData = input("Insert JSON raw content: ")
 dictData = json.loads(jsonData)
-encodedData = urllib.parse.urlencode(dictData)
+
+dictData['variables'] = json.dumps(dictData['variables'])
+
+encodedData = urllib.parse.urlencode(dictData, quote_via=urllib.parse.quote)
 
 print(encodedData)
